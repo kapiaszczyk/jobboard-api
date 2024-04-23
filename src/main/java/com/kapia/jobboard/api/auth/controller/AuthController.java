b7c49a73b096e0e752043c56fd2166fbdf935f9a
kapiaszczyk <41442206+kapiaszczyk@users.noreply.github.com>
2024-04-23 12:40:36 +0200

    feature: add openAPI documentation
    This closes #31

--- src/main/java/com/kapia/jobboard/api/auth/controller/AuthController.java
+++ src/main/java/com/kapia/jobboard/api/auth/controller/AuthController.java
@@ -4,7 +4,11 @@ import com.kapia.jobboard.api.auth.model.AppUser;
 import com.kapia.jobboard.api.auth.request.LoginRequest;
 import com.kapia.jobboard.api.auth.request.RegistrationRequest;
 import com.kapia.jobboard.api.auth.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Tag(name = "Authentication", description = "Authentication operations")
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private UserService authService;
@@ -28,12 +33,15 @@ public class AuthController {
         return authService.registerUser(appUser);
     }
 
-    @PostMapping("/login")
+    @Operation(summary = "Login as a user")
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, Object> login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
     }
 
-    @PostMapping("/user")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @Operation(summary = "Get user details")
+    @PostMapping(value = "/user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public AppUser getUserDetails() {
         return authService.getUserDetails();
     }
--- src/main/java/com/kapia/jobboard/api/data/controller/TechnologyController.java
+++ src/main/java/com/kapia/jobboard/api/data/controller/TechnologyController.java
@@ -3,6 +3,8 @@ package com.kapia.jobboard.api.data.controller;
 import com.kapia.jobboard.api.data.dto.TechnologyDTO;
 import com.kapia.jobboard.api.data.model.Technology;
 import com.kapia.jobboard.api.data.service.TechnologyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,8 +13,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Technology operations", description = "Retrieve and add technology information")
 @RestController
-@RequestMapping("/technology")
+@RequestMapping("/api/v1/technologies")
 public class TechnologyController {
 
     private final TechnologyService technologyService;
@@ -22,16 +25,19 @@ public class TechnologyController {
         this.technologyService = technologyService;
     }
 
+    @Operation(summary = "Get all technologies")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getAllTechnologies() {
         return ResponseEntity.status(HttpStatus.OK).body(technologyService.findAll());
     }
 
+    @Operation(summary = "Get a technology by id")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> create(@RequestBody List<Technology> technologies) {
         return ResponseEntity.status(HttpStatus.CREATED).body(technologyService.create(technologies));
     }
 
+    @Operation(summary = "Update a technology")
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> update(@RequestBody TechnologyDTO technologyDTO, @PathVariable(value = "id") long id) {
         return ResponseEntity.status(HttpStatus.OK).body(technologyService.update(technologyDTO, id));
